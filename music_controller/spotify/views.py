463e90159b2ae2b0cc1b4e8a12a92ab666a76f1a
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2021-02-16 16:32:46 -0300

    início da criação de uma requisição api para pegar a música que o usuário está ouvindo na conta dele

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, redirect
-from .credentials import REDIRECT_URI, CLIENT_ID, CLIENT_SECRET
+from .credentials import REDIRECT_URI, CLIENT_SECRET, CLIENT_ID
 from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import update_or_create_user_tokens, is_spotify_authenticated
+from .util import update_or_create_user_tokens, is_spotify_authenticated, get_user_tokens
+from api.models import Room
+
 
 class AuthURL(APIView):
-	def get(self, request, format=None):
+	def get(self, request, fornat=None):
 		scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
 
-		url = Request("GET", 'https://accounts.spotify.com/authorize', params={
+		url = Request('GET', 'https://accounts.spotify.com/authorize', params={
 			'scope': scopes,
 			'response_type': 'code',
 			'redirect_uri': REDIRECT_URI,
-			'client_id': CLIENT_ID,
+			'client_id': CLIENT_ID
 		}).prepare().url
 
 		return Response({'url': url}, status=status.HTTP_200_OK)
@@ -24,12 +26,12 @@ def spotify_callback(request, format=None):
 	code = request.GET.get('code')
 	error = request.GET.get('error')
 
-	response = post('https://acounts.spotify.com/api/token', data={
-		'grant-type': 'authorization_code',
+	response = post('https://accounts.spotify.com/api/token', data={
+		'grant_type': 'authorization_code',
 		'code': code,
 		'redirect_uri': REDIRECT_URI,
 		'client_id': CLIENT_ID,
-		'client_secret': CLIENT_SECRET,
+		'client_secret': CLIENT_SECRET
 	}).json()
 
 	access_token = response.get('access_token')
@@ -42,13 +44,21 @@ def spotify_callback(request, format=None):
 		request.session.create()
 
 	update_or_create_user_tokens(
-		request.session.session_key, access_token, token_type, expires_in, refresh_token
-	)
+		request.session.session_key, access_token, token_type, expires_in, refresh_token)
 
 	return redirect('frontend:')
 
 
 class IsAuthenticated(APIView):
 	def get(self, request, format=None):
-		is_authenticated = is_spotify_authenticated(self.request.session.session_key)
+		is_authenticated = is_spotify_authenticated(
+			self.request.session.session_key)
 		return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+
+
+class CurrentSong(APIView):
+	def get(self, request, format=None):
+		room_code = self.request.session.get('room_code')
+		room = Room.objects.filter(code=room_code)[0]
+		host = room.host
+		endpoint = '/player/currently-playing'
