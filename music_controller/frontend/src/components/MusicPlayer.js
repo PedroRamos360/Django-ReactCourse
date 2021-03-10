18028961e30270e8c0d93dac76e577a6f67d2ae4
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2021-03-10 20:18:23 -0300

    pausar e continuar a música funcionando

--- music_controller/frontend/src/components/MusicPlayer.js
+++ music_controller/frontend/src/components/MusicPlayer.js
@@ -10,6 +10,22 @@ export default class MusicPlayer extends Component {
 		super(props);
 	}
 
+	pauseSong() {
+		const requestOptions = {
+			method: 'PUT',
+			headers: {'Content-Type': 'applications/json'}
+		};
+		fetch('/spotify/pause', requestOptions);
+	}
+
+	playSong() {
+		const requestOptions = {
+			method: 'PUT',
+			headers: {'Content-Type': 'applications/json'}
+		};
+		fetch('/spotify/play', requestOptions);
+	}
+
 	render() {
 		const songProgress = (this.props.time / this.props.duration) * 100;
 
@@ -17,17 +33,19 @@ export default class MusicPlayer extends Component {
 			<Card>
 				<Grid container alignItems='center'>
 					<Grid item align='center' xs={4}>
-						<img src={this.props.image_url} height='100%' width='100%' alt='Song Cover' />
+						<img src={this.props.image_url ?? 'https://images.unsplash.com/photo-1499415479124-43c32433a620?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=889&q=80'} height='100%' width='100%' alt='Song Cover' />
 					</Grid>
 					<Grid item align='center' xs={8}>
 						<Typography component='h5' variant='h5'>
-							{ this.props.title }
+							{ this.props.title ?? "No song playing" }
 						</Typography>
 						<Typography color='textSecondary' variant='subtitle1'>
-							{ this.props.artist }
+							{ this.props.artist ?? '' }
 						</Typography>
 						<div>
-							<IconButton>
+							<IconButton onClick={() => {
+								this.props.is_playing ? this.pauseSong() : this.playSong()
+							}}>
 								{ this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
 							</IconButton>
 							<IconButton>
